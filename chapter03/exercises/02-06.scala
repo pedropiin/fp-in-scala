9ff5a0c7f5d6a431c2d93080b63f4e0c4017f7ad
pedropiin <pedarosa04@gmail.com>
2025-04-04 21:51:44 -0300

    updating remote

--- chapter03/exercises/02-06.scala
+++ chapter03/exercises/02-06.scala
@@ -32,6 +32,21 @@ object List {
 		}
 	}
 
+	def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = {
+		as match {
+			case Nil => z
+			case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+		}
+	}
+
+	def sum2(ns: List[Int]) = {
+		foldRight(ns, 0)((x, y) => x + y)
+	}
+	
+	def product2(ns: List[Double]) = {
+		foldRight(ns, 1.0)(_ * _)
+	}
+
 	// Exercise 3.2
 	def tail[A](l: List[A]): List[A] = {
 		l match {
@@ -93,8 +108,12 @@ object List {
 		}
 	}
 
+	// Exercise 3.9
+	def length[A](as: List[A]): Int = {
+		foldRight(as, Nil)()
+	}
 
 	def main(args: Array[String]): Unit = {
-		println(init(List(5, 4, 3, 2, 1)))
+		println(foldRight(List(1, 2, 3), Nil)(Cons(_, _)))
 	}
 }
